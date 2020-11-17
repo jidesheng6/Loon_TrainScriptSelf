30e7bacbe459ba391e125d9a2cb18825e426e652
JdeshengKno <2732423664@qq.com>
2020-11-17 21:27:12 +0800

    Update GetLocation_ByShanfuCloud.js

--- 自用/GetLocation_ByShanfuCloud.js
+++ 自用/GetLocation_ByShanfuCloud.js
@@ -1,6 +1,7 @@
 function GetLocation()
 {
     YunShanFuFlag = $request.headers["X-Tingyun-Id"];
+    console.log($request.headers);
     if(typeof(YunShanFuFlag)!=undefined)
     {
     const CatchYunShanFuResponse = JSON.parse($response.body);
@@ -9,7 +10,6 @@ function GetLocation()
     const StreetInfo = LocationInfo.streetNumber;
     const SaveStoreVar = LocationInfo.district;
     var StreetNumber,StreetName,DetailsLocation,FinalMessage,YunShanFuFlag;
-    YunShanFuFlag = $request.headers["X-Tingyun-Id"];
     StreetNumber = StreetInfo.number;
     StreetName=StreetInfo.street;
     DetailsLocation = RegeoDetail["pois"][0].name;
