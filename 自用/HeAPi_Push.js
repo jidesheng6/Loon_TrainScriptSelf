71ddee13ce80b4663bf7b11bebd791a6f6eff243
JdeshengKno <2732423664@qq.com>
2020-11-17 19:17:30 +0800

    Update HeAPi_Push.js

--- 自用/HeAPi_Push.js
+++ 自用/HeAPi_Push.js
@@ -29,27 +29,33 @@ function GetCityLocalId(CityName)
 
 function GetWeatherInfo()
 {
-    const SearchCity="昆山";
-    GetCityLocalId(SearchCity)//StoreId:id
-    const LocaltionID = $persistentStore.read("id");
-    const QueryPar = `location=${LocaltionID}&key=${DevApiKey}`;
-    const FullGetUrl = NowWeatherUrl + QueryPar;
-    $httpClient.get(FullGetUrl,function(error,response,data)
+    const SearchCity=$persistentStore.read("NowLocation");
+    if(SearchCity==undefined)
     {
-        let JsonReturn = JSON.parse(data);
-        const NowJson = JsonReturn.now;
-        const Now_Tmp = NowJson.temp;
-        const Now_FeelTmp = NowJson.feelsLike;
-        const Now_text = NowJson.text;
-        const Now_WindDir = NowJson.windDir;
-        const Now_WindLev = NowJson.windScale;
-        const Now_humidity = NowJson.humidity;
-        const TempIcon = "℃";
-        const NofityMessage = 
-        `🌏您的城市当前天气情况为:${Now_text}\n☁️当前温度:${Now_Tmp}${TempIcon}\n🌟体感温度:${Now_FeelTmp}${TempIcon}\n🌠风向:${Now_WindDir}风 风力${Now_WindLev}级\n💨相对湿度${Now_humidity}%`
-        $notification.post("和风天气API-实况天气💥",``,NofityMessage);
-        console.log("\n和风天气API调试信息:"+data);
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        $notification.post("获取位置信息失败","","请使用云闪付APP获取位置后重试");
+    }
+    else
+    {
+        GetCityLocalId(SearchCity)//StoreId:id
+        const LocaltionID = $persistentStore.read("id");
+        const QueryPar = `location=${LocaltionID}&key=${DevApiKey}`;
+        const FullGetUrl = NowWeatherUrl + QueryPar;
+        $httpClient.get(FullGetUrl,function(error,response,data)
+        {
+            let JsonReturn = JSON.parse(data);
+            const NowJson = JsonReturn.now;
+            const Now_Tmp = NowJson.temp;
+            const Now_FeelTmp = NowJson.feelsLike;
+            const Now_text = NowJson.text;
+            const Now_WindDir = NowJson.windDir;
+            const Now_WindLev = NowJson.windScale;
+            const Now_humidity = NowJson.humidity;
+            const TempIcon = "℃";
+            const NofityMessage = 
+            `🌏您的城市当前天气情况为:${Now_text}\n☁️当前温度:${Now_Tmp}${TempIcon}\n🌟体感温度:${Now_FeelTmp}${TempIcon}\n🌠风向:${Now_WindDir}风 风力${Now_WindLev}级\n💨相对湿度${Now_humidity}%`
+            $notification.post("和风天气API-实况天气💥",``,NofityMessage);
+            console.log("\n和风天气API调试信息:"+data);
     })
+   }
 }
 GetWeatherInfo()
