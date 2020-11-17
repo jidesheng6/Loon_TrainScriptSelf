e8d8f03475eb10016adce69121d09971b807dc1a
JdeshengKno <2732423664@qq.com>
2020-11-17 19:14:08 +0800

    Create GetLocation_ByShanfuCloud.js

---
+++ 自用/GetLocation_ByShanfuCloud.js
@@ -0,0 +1,25 @@
+function GetLocation()
+{
+    YunShanFuFlag = $request.headers["X-Tingyun-Id"];
+    if(typeof(YunShanFuFlag)!=undefined)
+    {
+    const CatchYunShanFuResponse = JSON.parse($response.body);
+    const RegeoDetail = CatchYunShanFuResponse.regeocode;
+    const LocationInfo = RegeoDetail.addressComponent;
+    const StreetInfo = LocationInfo.streetNumber;
+    const SaveStoreVar = LocationInfo.district;
+    var StreetNumber,StreetName,DetailsLocation,FinalMessage,YunShanFuFlag;
+    YunShanFuFlag = $request.headers["X-Tingyun-Id"];
+    StreetNumber = StreetInfo.number;
+    StreetName=StreetInfo.street;
+    DetailsLocation = RegeoDetail["pois"][0].name;
+    FinalMessage = `您当前所在位置为${SaveStoreVar}${StreetName}${StreetNumber}${DetailsLocation}`;
+    $notification.post("云闪付通知","信息获取成功",FinalMessage);
+    $persistentStore.write(SaveStoreVar,"NowLocation");
+    }
+    else
+    {
+        //
+    }
+}
+GetLocation()
