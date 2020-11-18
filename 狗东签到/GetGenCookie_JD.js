292c4eaece33d720b95c4c798ab215121cb97052
JdeshengKno <2732423664@qq.com>
2020-11-18 19:16:40 +0800

    Create GetGenCookie_JD.js

---
+++ 狗东签到/GetGenCookie_JD.js
@@ -0,0 +1,20 @@
+function Get_JD_GenCookie()
+{
+    const Raw_Cookies = $request.headers["Cookie"];
+    const CookiePtKey_Index = Raw_Cookies.indexOf("pt_key");
+    if(CookiePtKey_Index!=-1)
+    {
+        $persistentStore.write(Raw_Cookies,"Gen_Cookie_Key");
+        $notification.post("狗东-获取Cookie成功","","狗东Cookie写入成功,需要复制请打开log日志进行复制");
+        console.log("狗东Cookie获取成功,请复制您的Cookie:"+Raw_Cookies);
+    }
+    else
+    {
+        console.log("非此API的Cookie,此Cookie无法在狗东金融通用");
+    }
+    $done();
+}
+Get_JD_GenCookie()
+//MITM:api.m.jd.com
+//REGEX:https:\/\/api.m.jd.com\/client.action
+//StoreKey:Gen_Cookie_Key
