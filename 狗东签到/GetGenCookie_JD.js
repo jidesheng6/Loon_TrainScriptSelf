4cefc2fa89fe0cf12588592187280a5a33ca5805
JdeshengKno <2732423664@qq.com>
2020-11-21 00:44:26 +0800

    Update GetGenCookie_JD.js

--- 狗东签到/GetGenCookie_JD.js
+++ 狗东签到/GetGenCookie_JD.js
@@ -4,6 +4,7 @@ function Get_JD_GenCookie()
     const CookiePtKey_Index = Raw_Cookies.indexOf("pt_key");
     if(CookiePtKey_Index!=-1&&$request.url.indexOf("?functionId")==-1)
     {
+        console.log($request.url);
         $persistentStore.write(Raw_Cookies,"Gen_Cookie_Key");
         $notification.post("狗东-获取Cookie成功","","狗东Cookie写入成功,需要复制请打开log日志进行复制");
         console.log("狗东Cookie获取成功,请复制您的Cookie:"+Raw_Cookies);
