3f1460c43c47e040e5ebad224d4a41aebe1f0174
JdeshengKno <2732423664@qq.com>
2020-11-21 01:01:37 +0800

    Update Mutil_Model_Sign.js

--- 狗东签到/Mutil_Model_Sign.js
+++ 狗东签到/Mutil_Model_Sign.js
@@ -236,8 +236,8 @@ if(Cookies!=undefined)
         Ten_Street_Beans()//需要关闭请在前面加上//
         await Get_DoubleSign();//领取双签奖励
     }
-    //new Promise(Daily_Sign).then(Gold_Rong).then(Get_DoubleSign).then(Ten_Street_Beans)
-    AsyncFun();
+    new Promise(Daily_Sign).then(Gold_Rong).then(Get_DoubleSign).then(Ten_Street_Beans)
+    //AsyncFun();
 }
 else
 {
