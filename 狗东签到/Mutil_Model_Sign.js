ccdb13eff54cef1b66af60129dd7773fcb69dd33
JdeshengKno <2732423664@qq.com>
2020-11-22 20:30:54 +0800

    Update Mutil_Model_Sign.js

--- 狗东签到/Mutil_Model_Sign.js
+++ 狗东签到/Mutil_Model_Sign.js
@@ -160,6 +160,7 @@ function Gold_Rong()
             {
                 case 0:
                     $notification.post("狗东金融-成功","狗东金融","任务成功");
+                    Get_DoubleSign();
                     break;
                 case 15:
                     $notification.post("狗东金融-提示","狗东金融","请勿重复执行任务");
@@ -227,6 +228,57 @@ function Get_DoubleSign()
         
     })
 }
+function Lotty()//每日签到
+{
+    const LottyUrl = 'https://api.m.jd.com/client.action?functionId=lotteryDraw&body=%7B%22actId%22%3A%22jgpqtzjhvaoym%22%2C%22appSource%22%3A%22jdhome%22%2C%22lotteryCode%22%3A%22wupll7s766xme4p63iqis3e5z2olgso5cosvj7bm5cszrxjcj5rpe4lg4ba6nhb6hiawzpccizuck%22%7D&appid=ld';
+    const Cookie = $persistentStore.read("Gen_Cookie_Key");
+    const Headers = {cookie:Cookie};
+    const Par = 
+    {
+        url:LottyUrl,
+        headers:Headers,
+    }
+    $httpClient.get(Par,function(error,res,data)
+    {
+        var ReturnJsonDat = JSON.parse(data);
+        if(ReturnJsonDat.code!="3")
+        {
+            if(typeof(ReturnJsonDat.errorCode)!=undefined)
+        {
+            let ReturnErrorCode = ReturnJsonDat.errorCode;
+            switch(ReturnErrorCode)
+            {
+                case "T215":
+                    $notification.post("京东-每日抽奖","","今日抽奖次数已用光");
+                    break;
+                default:
+                    $notification.post("京东-每日抽奖","","发生未定义错误,请查看Log日志");
+                    console.log(data);
+            }
+        }
+        else
+        {
+            var InfoData = ReturnJsonDat.data;
+            var winnerInfo = InfoData.isWinner;
+            switch(winnerInfo)
+            {
+                case "1":
+                    $notification.post("京东-每日抽奖","","很抱歉没有中奖哦");
+                default:
+                    $notification.post("京东-每日抽奖","","发生未定义的错误代码,请查看日志");
+                    console.log(data);
+            }
+        }
+
+        }
+        else
+        {
+            $notification.post("京东-每日抽奖","","您的Cookie已过期");
+        }
+        
+    })
+}
+
 if(Cookies!=undefined)
 {
     /*async function AsyncFun()
@@ -241,15 +293,9 @@ if(Cookies!=undefined)
         Daily_Sign();
         Gold_Rong();
         Ten_Street_Beans();
+        Lotty();
         resolve();
-        console.log(this)
     })
-    func.then(function(reslove,reject)
-              {
-        Get_DoubleSign();
-        
-    })
-    //AsyncFun();
 }
 else
 {
